1d4fff7c0a7b32e663a9e6b1e8be9176925df21f
ktakahiro <takahiroinjp@gmail.com>
2017-11-18 17:10:32 +0900

    cahnge message

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'line/bot'
 require 'json'
 
 def client
-  @client ||= Line::Bot::Client.new { |config|
+  @client ||= Line::Bot::Client.new {|config|
     config.channel_secret = 'b5b2790170b5d8650c57ffb65b2a51e3'
     config.channel_token = 'PAtJGMFyV7L2PNR4WZmRVjfXnTSZIQnY24c5bIEL0Y6qRqe9eYi8ZG0rFwe7LUZJyN4JOuhVxpgLvcb/aN49ztrlFlxwcjZUWR2BHTbxblwHDInGfAKgVGNJcc5is1Ixwp+tOne1DkRFChUIS0P/8gdB04t89/1O/w1cDnyilFU='
   }
@@ -14,42 +14,44 @@ post '/' do
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
-    error 400 do 'Bad Request' end
+    error 400 do
+      'Bad Request'
+    end
   end
 
   events = client.parse_events_from(body)
-  events.each { |event|
+  events.each {|event|
     if event['type'] == 'beacon' then
-          profile = client.get_profile(event['source']['userId']);
-          displayName = JSON.parse(profile.body)['displayName']
+      profile = client.get_profile(event['source']['userId']);
+      displayName = JSON.parse(profile.body)['displayName']
 
-          message = [
-            {
+      message = [
+          {
               type: 'sticker',
               packageId: 1,
               stickerId: 17
-            },
-            {
+          },
+          {
               type: 'text',
               text: 'こんにちは！' + displayName + 'さん'
-            }
-          ]
-          client.reply_message(event['replyToken'], message)
-        else
-          message = [
-            {
+          }
+      ]
+      client.reply_message(event['replyToken'], message)
+    else
+      message = [
+          {
               type: 'text',
-              text: '「こんにちは」と呼びかけて下さいね！'
-            },
-            {
+              text: 'さようなら！'
+          },
+          {
               type: 'sticker',
               packageId: 1,
               stickerId: 4
-            }
-          ]
-          client.reply_message(event['replyToken'], message)
-        end
-     
+          }
+      ]
+      client.reply_message(event['replyToken'], message)
+    end
+
   }
   "OK"
 
