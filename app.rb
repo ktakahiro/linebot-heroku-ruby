3466c651a7835ada2a47ce400853c5e3b5246559
ktakahiro <takahiroinjp@gmail.com>
2017-11-18 17:26:20 +0900

    fixed url message

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'line/bot'
 require 'json'
 
 def client
-  @client ||= Line::Bot::Client.new {|config|
+  @client ||= Line::Bot::Client.new do |config|
     config.channel_secret = 'b5b2790170b5d8650c57ffb65b2a51e3'
     config.channel_token = 'PAtJGMFyV7L2PNR4WZmRVjfXnTSZIQnY24c5bIEL0Y6qRqe9eYi8ZG0rFwe7LUZJyN4JOuhVxpgLvcb/aN49ztrlFlxwcjZUWR2BHTbxblwHDInGfAKgVGNJcc5is1Ixwp+tOne1DkRFChUIS0P/8gdB04t89/1O/w1cDnyilFU='
-  }
+  end
 end
 
 post '/' do
@@ -20,43 +20,41 @@ post '/' do
   end
 
   events = client.parse_events_from(body)
-  events.each {|event|
-    if event['type'] == 'beacon' then
-      profile = client.get_profile(event['source']['userId']);
+  events.each do |event|
+    if event['type'] == 'beacon'
+      profile = client.get_profile(event['source']['userId'])
       displayName = JSON.parse(profile.body)['displayName']
 
       message = [
-          {
-              type: 'sticker',
-              packageId: 1,
-              stickerId: 17
-          },
-          {
-              type: 'text',
-              text: 'ようこそ奥多摩へ！' + displayName + 'さん'
-          },
-          {
-              type: 'text',
-              text: 'https://www.youtube.com/watch?v=c8qvbu11fps'
-      }
+        {
+          type: 'sticker',
+          packageId: 1,
+          stickerId: 17
+        },
+        {
+          type: 'text',
+          text: 'ようこそ奥多摩へ！' + displayName + 'さん'
+        },
+        {
+          type: 'text',
+          text: 'https://www.youtube.com/watch?v=c8qvbu11fps'
+        }
       ]
       client.reply_message(event['replyToken'], message)
     else
       message = [
-          {
-              type: 'text',
-              text: 'さようなら！'
-          },
-          {
-              type: 'sticker',
-              packageId: 1,
-              stickerId: 4
-          }
+        {
+          type: 'text',
+          text: 'さようなら！'
+        },
+        {
+          type: 'sticker',
+          packageId: 1,
+          stickerId: 4
+        }
       ]
       client.reply_message(event['replyToken'], message)
     end
-
-  }
-  "OK"
-
+  end
+  'OK'
 end
